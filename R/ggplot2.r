610b0bfb39660f63bc84a1006af2d0d4e2ca26fe
baptiste <baptiste.auguie@gmail.com>
2016-03-13 18:04:36 +1300

    skeleton

---
+++ R/ggplot2.r
@@ -0,0 +1,13 @@
+## this is where we extract relevant grobs from a ggplot2 legend
+## and store its mapping to maintain a link between data and glyph
+
+
+#' Extract legend grobs
+#'
+#' @param p ggplot
+#' @return list with mapping metadata
+#' @export
+extract_legend_grobs <- function(p){
+
+
+}
---
+++ R/markdown.r
@@ -0,0 +1,16 @@
+## this is where we brew markdown output
+## to be post-processed by pandoc into latex or html
+
+
+#' Caption
+#'
+#' @param template text with placeholders for each glyph
+#' @param glyphs list of glyphs with mapping metadata
+#' @param out optional output file name
+#'
+#' @return markdown
+#' @export
+caption <- function(template, glyphs, out = NULL){
+
+
+}

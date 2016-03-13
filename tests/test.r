6138ced1b56a1d7bbc491bd518592635846cb63e
baptiste <baptiste.auguie@gmail.com>
2016-03-13 19:09:50 +1300

    readme

--- tests/test.r
+++ tests/test.r
@@ -11,17 +11,19 @@ g <- extract_legend_grobs(p)
 
 grid.arrange(grobs=unlist(g$grobs, recursive=FALSE))
 
-tg <- lapply(c("Figure 1. Those points I'm making",
-               "are important, ", "nonetheless", "and", "have value too", "I believe."),
+tg <- lapply(c(expression(bold("Figure 1.")),"Those points","I'm making",
+               "are important,", "nonetheless", "and", "have value too", ", I believe."),
              textGrob)
 pGrob <- function(fill, size=1, ...){
   rectGrob(..., width=unit(size,"line"), height=unit(size,"line"), gp=gpar(fill=fill))
 }
 
 pg <- mapply(pGrob, fill=1:5, size=0.5, SIMPLIFY = FALSE)
-
+pg <- unlist(g$grobs, recursive=FALSE)
 bg <- interleave_grobs(tg, unlist(g$grobs, recursive=FALSE))
+bg <- list(tg[[1]], tg[[2]], pg[[1]], pg[[2]], tg[[3]], tg[[4]],
+           tg[[5]], pg[[3]], pg[[4]], tg[[6]], pg[[5]], tg[[7]])
 
-test <- captionGrob(lg=bg, width = unit(3, "in"))
+test <- captionGrob(lg=bg, width = unit(5, "in"))
 grid.newpage()
-grid.arrange(p+theme(legend.position="none"), test, ncol=1)
+grid.arrange(p+theme(legend.position="none"), test, heights=c(5,1))
